5de6ea06da28291bbd175c1e29701bbedf396454
Junevy <junev.chung@gmail.com>
2026-07-06 00:27:24 +0800

    bug fixed: Add  height seperately for ToggleButton to prevent the icon of left from being stretched when setting width or height.

--- Controls/Button/ToggleButton.xaml.cs
+++ Controls/Button/ToggleButton.xaml.cs
@@ -19,6 +19,22 @@ namespace Junevy.Controls.Controls.Button
         }
         public static readonly DependencyProperty DisplayModeProperty =
             DependencyProperty.Register("DisplayMode", typeof(ShapeMode), typeof(ToggleButton), new PropertyMetadata(ShapeMode.Rectangular));
+
+        public double SwitchWidth
+        {
+            get { return (double)GetValue(SwitchWidthProperty); }
+            set { SetValue(SwitchWidthProperty, value); }
+        }
+        public static readonly DependencyProperty SwitchWidthProperty =
+            DependencyProperty.Register(nameof(SwitchWidth), typeof(double), typeof(ToggleButton), new PropertyMetadata(40.0));
+
+        public double SwitchHeight
+        {
+            get { return (double)GetValue(SwitchHeightProperty); }
+            set { SetValue(SwitchHeightProperty, value); }
+        }
+        public static readonly DependencyProperty SwitchHeightProperty =
+            DependencyProperty.Register(nameof(SwitchHeight), typeof(double), typeof(ToggleButton), new PropertyMetadata(20.0));
     }
 
 
